a463def8d956a98bf4dc595237d04ee519f8b0c8
lztxi <72137239+lztxi@users.noreply.github.com>
2024-07-09 15:47:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 sed -i 's/=6.1/=5.15/g' target/linux/mediatek/Makefile
-sed -i 's/=6.6/=6.1/g' target/linux/mediatek/Makefile
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
