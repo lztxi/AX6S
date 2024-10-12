fd74d9ac5e076248e5cf5b4e693ce98483cdd69f
lztxi <72137239+lztxi@users.noreply.github.com>
2024-10-12 10:13:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-echo 'src-git OpenClash https://github.com/lztxi/OpenClash' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/lztxi/OpenClash.git;dev' >>feeds.conf.default
